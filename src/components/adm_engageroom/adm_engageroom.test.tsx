167f9bce4a820a81156ba224020458ce5156e84f
NwankwoKc <nkelechi21@gmail.com>
2026-07-31 21:34:48 +0100

    fix:removed unused dep from adm_engageroom component

--- src/components/adm_engageroom/adm_engageroom.test.tsx
+++ src/components/adm_engageroom/adm_engageroom.test.tsx
@@ -1,11 +1,9 @@
 import { describe, test, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
-import adm_engageroom from './adm_engageroom';
 
 describe('<adm_engageroom />', () => {
   test('should mount', () => {
-    render(<adm_engageroom />);
 
     const admEngageroom = screen.getByTestId('adm_engageroom');
 
